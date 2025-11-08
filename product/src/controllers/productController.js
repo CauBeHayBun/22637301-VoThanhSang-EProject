6ca8fbf4883fbc483a8f51da04822560a5abf475
CauBeHayBun <vosang348@gmail.com>
2025-11-08 12:59:30 +0700

    noww

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -109,13 +109,11 @@ class ProductController {
             res.status(500).json({ message: "Server error" });
         }
     }
-
-
-    // async getProductsById(req, res) {
-    //     const { id } = req.params;
-    //     const product = await Product.findById(id);
-    //     res.status(200).json(product);
-    // }
+    async getProductsById(req, res) {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    }
 }
 
 module.exports = ProductController;
